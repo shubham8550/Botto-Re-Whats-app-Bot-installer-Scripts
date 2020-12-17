1a2c655162812e38915aaf0bea3d8f01298ff954
Shubham Badgujar <shubhambadgujar222@gmail.com>
2020-12-17 11:55:13 +0530

    revision

--- BottoReWindows_X64.bat
+++ BottoReWindows_X64.bat
@@ -5,20 +5,23 @@ cd Whatsapp-Botto-Re || goto :error
 npm install || goto :error
 
 echo ------Bot Nodejs dependancys Successfully Installed ------
+:bin1
 echo ------Installing Required bin's                     ------
 where mogrify.exe >nul 2>&1 && echo: || (echo `Downloading and Installing ImageMagic (Install it with Legacy components eg mogrify.exe or it wont work` && GOTO :installmagick )
 :bin2
-powershell -Command "Invoke-WebRequest https://imagemagick.org/download/binaries/ImageMagick-7.0.10-50-Q16-HDRI-x64-dll.exe -OutFile magicksetup.exe && ./magicksetup.exe"
+powershell -Command "Invoke-WebRequest https://github.com/shubham8550/Botto-Re-Whats-app-Bot-installer-Scripts/raw/master/gif2webp.exe -OutFile gif2webp.exe"
 
 
-
+echo ------[ DONE ]Botto re   Successfully Installed ------
+echo ------[ note ] you can run bot using command  'npm start'
 
 GOTO EOF
 :error
 echo Failed with error #%errorlevel%.
 exit /b %errorlevel%
 :installmagick
-powershell -Command "Invoke-WebRequest https://imagemagick.org/download/binaries/ImageMagick-7.0.10-50-Q16-HDRI-x64-dll.exe -OutFile magicksetup.exe && ./magicksetup.exe"
+powershell -Command "Invoke-WebRequest https://imagemagick.org/download/binaries/ImageMagick-7.0.10-50-Q16-HDRI-x64-dll.exe -OutFile magicksetup.exe"
+magicksetup.exe
 GOTO :bin2
 :botstart
 npm start

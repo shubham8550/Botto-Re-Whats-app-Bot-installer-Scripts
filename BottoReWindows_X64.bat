05d0fd253fa6d28498b2d0615493c115141aa88f
Shubham Badgujar <shubhambadgujar222@gmail.com>
2020-12-17 12:17:16 +0530

    riv3

--- BottoReWindows_X64.bat
+++ BottoReWindows_X64.bat
@@ -1,20 +1,29 @@
 @echo off
+CLS
 where git.exe >nul 2>&1 && echo: || echo git not installed
 git clone https://github.com/SomnathDas/Whatsapp-Botto-Re || goto :error
 cd Whatsapp-Botto-Re || goto :error
-rem npm install || goto :error
-
+npm install || goto :error
+echo node start > "Run.bat"
 echo ------Bot Nodejs dependancys Successfully Installed ------
 :bin1
 echo ------Installing Required bin's                     ------
 where mogrify.exe >nul 2>&1 && echo: || (echo `Downloading and Installing ImageMagic (Install it with Legacy components eg mogrify.exe or it wont work` && GOTO :installmagick )
 :bin2
+echo ------Installing gif2webp                  ------
 powershell -Command "Invoke-WebRequest https://github.com/shubham8550/Botto-Re-Whats-app-Bot-installer-Scripts/raw/master/gif2webp.exe -OutFile gif2webp.exe"
 
 
 echo ------[ DONE ]Botto re   Successfully Installed ------
-echo ------[ note ] you can run bot using command  'npm start'
+echo ------[ note ] you can Start bot with `Run.bat` ------
 
+ECHO.
+ECHO 1 - Run Bot Now?
+ECHO 2 - exit
+ECHO.
+SET /P M=Type 1, 2, 3, or 4 then press ENTER:
+IF %M%==1 GOTO botstart
+IF %M%==2 GOTO EOF
 GOTO EOF
 :error
 echo Failed with error #%errorlevel%.
